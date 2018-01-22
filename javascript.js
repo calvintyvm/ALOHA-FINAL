d2510076a0a01cf5f9ac76c5258e9bb31d513498
Calvin <calvinting@github.com>
2018-01-22 07:26:57 -0800

    aa

--- javascript.js
+++ javascript.js
@@ -42,3 +42,32 @@ $('.carousel').flickity({
         }
       }
     });
+
+
+
+  $(document).ready(function(e) {
+      $('#btnValidate').click(function() {
+          var sEmail = $('#txtEmail').val();
+          if ($.trim(sEmail).length == 0) {
+              alert('Please enter valid email address');
+              e.preventDefault();
+          }
+          if (validateEmail(sEmail)) {
+              alert('Thanks for subscribing!');
+          }
+          else {
+              alert('Invalid Email Address');
+              e.preventDefault();
+          }
+      });
+  });
+  
+  function validateEmail(sEmail) {
+      var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+      if (filter.test(sEmail)) {
+          return true;
+      }
+      else {
+          return false;
+      }
+  }
